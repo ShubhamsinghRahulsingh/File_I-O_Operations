dcc60896e23c2b78f7f60d9661fd35f7594d2135
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-12-11 00:23:32 +0530

    (Shubham)Add: Converting String format to Json Format and vice versa

--- File_IO_Operations/Program.cs
+++ File_IO_Operations/Program.cs
@@ -9,12 +9,13 @@ namespace File_IO_Operations
         {
             FileOperation file = new FileOperation();
             Binary binary = new Binary();
+            JSon jSon = new JSon();
             Console.WriteLine("Welcome to Perform Operations on File");
             bool flag=true;
             while(flag)
             {
                 Console.WriteLine("-----------------------------------------------------");
-                Console.WriteLine("Select From the Following\n1.FileExistsOrNot\n2.ReadAllLines\n3.ReadAllText\n4.CopyFile\n5.DeleteFile\n6.ReadFromStreamReader\n7.WriteFromStreamWriter\n8.BinarySerialization\n9.BinaryDeserialization\n10.Exit");
+                Console.WriteLine("Select From the Following\n1.FileExistsOrNot\n2.ReadAllLines\n3.ReadAllText\n4.CopyFile\n5.DeleteFile\n6.ReadFromStreamReader\n7.WriteFromStreamWriter\n8.BinarySerialization\n9.BinaryDeserialization\n10.JsonSerializaion\n11.JsonDeSerialization\n12.Exit");
                 int choice=Convert.ToInt32(Console.ReadLine());
                 switch(choice)
                 {
@@ -46,6 +47,12 @@ namespace File_IO_Operations
                         binary.BinaryDeserialization();
                         break;
                     case 10:
+                        jSon.JsonSerialize();
+                        break;
+                    case 11:
+                        jSon.JsonDeSerialize();
+                        break;
+                    case 12:
                         flag = false;
                         break;
                 }
---
+++ File_IO_Operations/SerializationAndDeserialization/JSon.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_IO_Operations.SerializationAndDeserialization
+{
+    public class JSon
+    {
+        public void JsonSerialize()
+        {
+            BlogSites bsObject = new BlogSites()
+            {
+                Name = "Shubham",
+                Description = "Welcome to the coding"
+            };
+            //converting Blogsites object to json string format
+            string jsonData = JsonConvert.SerializeObject(bsObject);
+            Console.WriteLine(jsonData);
+        }
+        public void JsonDeSerialize()
+        {
+            string json = @"{
+            
+                'Name':'Shubham',
+                'Description':'Welcome to the coding'
+            }";
+            //converting json string format to Blogsites object
+            BlogSites bsObject = JsonConvert.DeserializeObject<BlogSites>(json);
+            Console.WriteLine("Name:"+bsObject.Name+"\n"+"Description:"+ bsObject.Description);
+        }
+
+    }
+    [DataContract]
+    public class BlogSites
+    {
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string Description { get; set; }
+    }
+}
