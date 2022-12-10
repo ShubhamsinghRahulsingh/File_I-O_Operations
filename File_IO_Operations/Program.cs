d9706a3dead08cfa25eebe3e0ddd03b65a67a4e9
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-12-10 17:42:12 +0530

    (Shubham)Add: Project Folder structure Created

---
+++ File_IO_Operations/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace File_IO_Operations
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Perform Operations on File");
+        }
+    }
+}
