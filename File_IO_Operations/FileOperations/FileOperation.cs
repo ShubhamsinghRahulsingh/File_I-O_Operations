310938bf57fc37f2c590e5658ccc852b3ebbed44
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-12-10 18:25:06 +0530

    (Shubham)Add: checking Given file Exists or not

---
+++ File_IO_Operations/FileOperations/FileOperation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_IO_Operations.FileOperations
+{
+    public class FileOperation
+    {
+        string filePath = @"D:\GitRepository\File_IO_Operations\File_IO_Operations\File\operation.txt";
+        //UC1
+        public void FileExists()
+        {
+            if (File.Exists(filePath))
+            {
+                Console.WriteLine("Given File Exists");
+            }
+            else
+                Console.WriteLine("File Does not Exists");
+        }
+
+    }
+}
--- File_IO_Operations/Program.cs
+++ File_IO_Operations/Program.cs
@@ -1,11 +1,29 @@
-﻿using System;
+﻿using File_IO_Operations.FileOperations;
+using System;
 namespace File_IO_Operations
 {
     class Program
     {
         public static void Main(string[] args)
         {
+            FileOperation file = new FileOperation();
             Console.WriteLine("Welcome to Perform Operations on File");
+            bool flag=true;
+            while(flag)
+            {
+                Console.WriteLine("-----------------------------------------------------");
+                Console.WriteLine("Select From the Following\n1.FileExistsOrNot\n2.Exit");
+                int choice=Convert.ToInt32(Console.ReadLine());
+                switch(choice)
+                {
+                    case 1:
+                        file.FileExists();
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
